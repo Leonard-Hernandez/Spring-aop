4f44eb05a08ff0127f8117838b126f068a11044a
Leonard <Leonardhernandez02@gmail.com>
2025-02-10 22:45:49 -0500

    usando around

--- src/main/java/com/leonard/curso/springboot/app/aop/springboot_aop/aop/GreetingAspect.java
+++ src/main/java/com/leonard/curso/springboot/app/aop/springboot_aop/aop/GreetingAspect.java
@@ -3,9 +3,11 @@ package com.leonard.curso.springboot.app.aop.springboot_aop.aop;
 import java.util.Arrays;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
@@ -46,6 +48,24 @@ public class GreetingAspect {
         String method = joinPoint.getSignature().getName();
         String args = Arrays.toString(joinPoint.getArgs());
         logger.info("afterThrowing method: " + method + ", args: " + args);
+    }
+
+    @Around("execution(String com.leonard.curso.springboot.app.aop.springboot_aop.services.GreetingService.*(..))")
+    public Object loggerAround(ProceedingJoinPoint joinPoint) throws Throwable{
+        String method = joinPoint.getSignature().getName();
+        String args = Arrays.toString(joinPoint.getArgs());
+
+        Object result = null;
+
+        try {
+            logger.info("the method: " + method + ", args: " + args);
+            result = joinPoint.proceed();
+            logger.info("the method: " + method + " return" + result);
+            return result;
+        } catch (Throwable e) {
+            logger.error("Error in method: " + method + ", args: " + args, e);
+            throw e;
+        }
     }
 
 }
